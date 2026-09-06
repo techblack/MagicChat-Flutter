43df4f225318a7df4ba149d84d139fa13db31227
techblack <13714126+techblack@users.noreply.github.com>
2026-09-06 23:19:46 +0800

    chore: prepare v0.3.22 release

--- lib/data/update_service.dart
+++ lib/data/update_service.dart
@@ -56,8 +56,8 @@ class UpdateService {
   static const updateSource = String.fromEnvironment('MAGICCHAT_UPDATE_SOURCE',
       defaultValue:
           String.fromEnvironment('UPDATE_SOURCE', defaultValue: 'release'));
-  static const currentBuild = 34;
-  static const currentVersion = '0.3.21';
+  static const currentBuild = 35;
+  static const currentVersion = '0.3.22';
 
   static String get manifestUrl =>
       updateSource == 'release' ? releaseManifestUrl : updateSource;
